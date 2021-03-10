ad266f677fb1af8f322b175a337a7dd8101a7818
Gallium Wang <galliumwang199@gmail.com>
2021-03-10 15:54:00 +0800

    add image resize

---
+++ image_resize.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+from PIL import Image
+import PIL.Image
+
+s3_client = boto3.client('s3')
+
+def resize_image(image_path, resized_path):
+    with Image.open(image_path) as image:
+        image.thumbnail(tuple(x / 2 for x in image.size))
+        image.save(resized_path)
